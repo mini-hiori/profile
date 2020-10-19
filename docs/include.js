73f59c059a3739000031172825cdb93c660eb929
mini-hiori18 <phantombisco@gmail.com>
2020-10-20 06:19:47 +0900

    include test

--- docs/include.js
+++ docs/include.js
@@ -1,14 +1,52 @@
-// https://qiita.com/hiroyukiwk/items/f2a74ba1406de9fad6f6
+// https://kenpoco.hatenadiary.org/entry/20080501/1209636103
+function include(filename, afterfunc) {
 
-function header(rootDir){
-    $.ajax({
-        url: rootDir + "include/header.html", // ディレクトリー変更
-        cache: false,
-        async: false,
-        dataType: 'html',
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir); 
-            document.write(html);
+    include.seq = (include.seq)? include.seq + 1: 1;
+  
+    var id = new Date().getTime() + "-" + include.seq;
+    var inc = document.createElement("iframe");
+  
+    inc.id = "inc-" + id;
+    inc.src = filename;
+    inc.style.display = "none";
+    document.write("<span id=\"" + id + "\"></span>");
+      
+    var incfunc = function() {
+      
+      var s = (function() {
+        var suffix = (n = filename.lastIndexOf(".")) >= 0 ? filename.substring(n): "default";
+        if (suffix == ".html") return inc.contentWindow.document.body.innerHTML;
+        if (suffix == ".txt") return inc.contentWindow.document.body.firstChild.innerHTML;
+        if (suffix == "default") return inc.contentWindow.document.body.innerHTML;
+      })();
+  
+      var span = document.getElementById(id);
+  
+      var insertBeforeHTML = function(htmlStr, refNode) {
+        if (document.createRange) {
+          var range = document.createRange();
+          range.setStartBefore(refNode);
+          refNode.parentNode.insertBefore(range.createContextualFragment(htmlStr), refNode);
+        } else {
+          refNode.insertAdjacentHTML('BeforeBegin', htmlStr);
         }
-    });
-}
+      };
+  
+      insertBeforeHTML(s.split("&gt;").join(">").split("&lt;").join("<"), span);
+      document.body.removeChild(inc);
+      span.parentNode.removeChild(span);
+      if (afterfunc) afterfunc();
+    };
+  
+    if (window.attachEvent) {
+      window.attachEvent('onload', 
+        function() {
+          document.body.appendChild(inc); 
+          inc.onreadystatechange = function() { if (this.readyState == "complete") incfunc(); };
+        });
+    }
+    else {
+      document.body.appendChild(inc);
+      inc.onload = incfunc;
+    }
+  }
